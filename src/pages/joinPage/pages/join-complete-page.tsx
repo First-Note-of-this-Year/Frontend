07943a5a71c9cd0288bbf50c875913aa51ea330a
Kwon DaeGeun <dk31415@naver.com>
2025-12-23 16:17:32 +0900

    fix: join-complete 네이게이트 수정

--- src/pages/joinPage/pages/join-complete-page.tsx
+++ src/pages/joinPage/pages/join-complete-page.tsx
@@ -1,32 +1,20 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CDPlayerImage from "@/assets/obj_cdplayer.webp";
 import { NavigationButton } from "@/components/ui/navigation-button";
 import { PageLayout } from "@/components/ui/page-layout";
-import { ROUTES } from "@/constants/routes";
 import { useAuthStore } from "@/stores/useAuthStore";
 
 export default function JoinCompletePage() {
   const navigate = useNavigate();
-  const { checkAuth } = useAuthStore();
-  const [isLoading, setIsLoading] = useState(false);
+  const { isLoggedIn } = useAuthStore();
 
-  const handleClick = async () => {
-    setIsLoading(true);
-    try {
-      const authCheckResult = await checkAuth();
-      if (authCheckResult) {
-        navigate(ROUTES.BOARD);
-      } else {
-        navigate(ROUTES.HOME);
-      }
-    } catch (error) {
-      console.error("Failed to check auth:", error);
-      navigate(ROUTES.HOME);
-    } finally {
-      setIsLoading(false);
+  const handleToBoard = () => {
+    if (!isLoggedIn) {
+      navigate("/");
+      return;
     }
-  };
+    navigate("/board");
+  }
 
   return (
     <PageLayout
@@ -42,11 +30,9 @@ export default function JoinCompletePage() {
       bottomContent={
         <NavigationButton
           className="w-full"
-          active={!isLoading}
-          disabled={isLoading}
-          onClick={handleClick}
+          onClick={handleToBoard}
         >
-          {isLoading ? "확인 중..." : "다음으로"}
+          다음으로
         </NavigationButton>
       }
     >
